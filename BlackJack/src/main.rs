ab377f18db889f6f2069bce5dce18050c605529f
ErikRBjorklund <erbjorklund@icloud.com>
2022-01-03 20:14:35 -0700

    Minor Style Changes
    Fixed ()'s on if thens since I am a java kid

--- BlackJack/src/main.rs
+++ BlackJack/src/main.rs
@@ -6,7 +6,7 @@ use std::io;
 fn main() {
     println!("What is your name, Player?");
     let stdin = io::stdin();
-    let mut name = stdin.lock().lines().next().unwrap().unwrap();
+    let name = stdin.lock().lines().next().unwrap().unwrap();
 
     println!("Welcome, {}!", name);
 
@@ -18,32 +18,32 @@ fn main() {
         playerHand.push(getCard(rand::thread_rng().gen_range(1..14)));
         computerHand.push(getCard(rand::thread_rng().gen_range(1..14)));
         computerHand.push(getCard(rand::thread_rng().gen_range(1..14)));
-        if (getScore(&playerHand) != 21) {
+        if getScore(&playerHand) != 21 {
             loop {
                 println!("{}'s Hand: {:?}", name, playerHand);
                 println!("{}'s Score: {}", name, getScore(&playerHand));
                 println!("Dealer's Showing Card: {}", computerHand[0]);
                 println!("Would you like to [hit/hold]?");
                 let answer = stdin.lock().lines().next().unwrap().unwrap();
-                if (answer.eq("hit")) {
+                if answer.eq("hit") {
                     playerHand.push(getCard(rand::thread_rng().gen_range(1..14)));
                     let score = getScore(&playerHand);
-                    if (score == 21) {
+                    if score == 21 {
                         println!("BlackJack!! Congratulations {}, you won!", name);
                         break;
-                    } else if (score > 21) {
+                    } else if score > 21 {
                         println!("Sorry {}, you lost with a score of {}! You're bound to win next time!", name, score);
                         break;
                     }
                 } else {
-                    while (getScore(&computerHand) < 17) {
+                    while getScore(&computerHand) < 17 {
                         computerHand.push(getCard(rand::thread_rng().gen_range(1..14)));
                     }
-                    if (getScore(&computerHand) > 21) {
+                    if getScore(&computerHand) > 21 {
                         println!("Congratulations {}! You WON!", name);
-                    } else if (getScore(&computerHand) < getScore(&playerHand)) {
+                    } else if getScore(&computerHand) < getScore(&playerHand) {
                         println!("Congratulations {}! You WON!", name);
-                    } else if (getScore(&computerHand) > getScore(&playerHand)) {
+                    } else if getScore(&computerHand) > getScore(&playerHand) {
                         println!("Sorry {}, you lost with a score of {}! You're bound to win next time!", name, getScore(&playerHand));
                     } else {
                         println!("You tied! Almost gottem!");
@@ -62,23 +62,23 @@ fn main() {
 
 
         println!("Would you like to play again? [yes/no]");
-        if (stdin.lock().lines().next().unwrap().unwrap().eq("no")) {
+        if stdin.lock().lines().next().unwrap().unwrap().eq("no") {
             break;
         }
     }
 }
 
 fn getCard(num: i8) -> String {
-    if (num <= 10 && num != 1) {
+    if num <= 10 && num != 1 {
         return num.to_string();
     } 
-    if (num == 11) {
+    if num == 11 {
         return "J".to_string();
     }
-    if (num == 12) {
+    if num == 12 {
         return "Q".to_string();
     }
-    if (num == 13) {
+    if num == 13 {
         return "K".to_string();
     }
     return "A".to_string();
@@ -88,43 +88,43 @@ fn getScore(hand: &Vec<String>) -> i8 {
     let mut score: i8 = 0;
     let mut aces: i8 = 0;
     for card in hand.iter() {
-        if (card.eq("2")) {
+        if card.eq("2") {
             score += 2;
-        } else if (card.eq("3")) {
+        } else if card.eq("3") {
             score += 3;
-        } else if (card.eq("4")) {
+        } else if card.eq("4") {
             score += 4;
-        } else if (card.eq("5")) {
+        } else if card.eq("5") {
             score += 5;
-        } else if (card.eq("6")) {
+        } else if card.eq("6") {
             score += 6;
-        } else if (card.eq("7")) {
+        } else if card.eq("7") {
             score += 7;
-        } else if (card.eq("8")) {
+        } else if card.eq("8") {
             score += 8;
-        } else if (card.eq("9")) {
+        } else if card.eq("9") {
             score += 9;
-        } else if (card.eq("10")) {
+        } else if card.eq("10") {
             score += 10;
-        } else if (card.eq("J")) {
+        } else if card.eq("J") {
             score += 10;
-        } else if (card.eq("Q")) {
+        } else if card.eq("Q") {
             score += 10;
-        } else if (card.eq("K")) {
+        } else if card.eq("K") {
             score += 10;
-        } else if (card.eq("A")) {
+        } else if card.eq("A") {
             aces += 1;
         }
     }
-    if (aces == 1) {
-        if (21 - score >= 11) {
+    if aces == 1 {
+        if 21 - score >= 11 {
             score += 11;
         } else {
             score += 1;
         }
-    } else if (aces > 1) {
+    } else if aces > 1 {
         score += aces - 1;
-        if (21 - score >= 11) {
+        if 21 - score >= 11 {
             score += 11;
         } else {
             score += 1;
